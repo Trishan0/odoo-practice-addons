cce6bc8d063e2d0d36d6d2dbe36a8e3f02c09cf5
Trishan <sanjanatrishan@gmail.com>
2025-08-10 15:23:37 +0530

    [ADD] the module base structre

---
+++ master-equipments/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Master Equipments",
+    "version": "1.0",
+    "summary": "Manage master equipment records",
+    "sequence": 20,
+    "description": """
+This module allows you to manage master equipment records, including their details and associated information.
+    """,
+
+    "category": "Inventory/Equipment Management",
+
+    'installable': True,
+    'application': True,
+
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+
+    ]
+}
