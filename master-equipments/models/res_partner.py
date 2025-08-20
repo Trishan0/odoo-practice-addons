dcc9ae322b8a58b5bc7d0ddd5282b7d2f9eb94f0
Trishan <sanjanatrishan@gmail.com>
2025-08-20 23:02:37 +0530

    [ADD] Mobile number validation and formatting in `res.partner`
    - Added constraints to validate mobile numbers for correct format and length.
    - Implemented auto-formatting for mobile numbers during create and write operations.

--- master-equipments/models/res_partner.py
+++ master-equipments/models/res_partner.py
@@ -1,9 +1,44 @@
-from email.policy import default
+import re
+import typing
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.api import ValuesType, Self
+from odoo.exceptions import ValidationError
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     is_filter_po_products_category = fields.Boolean(string="Filter Products in PO based on Category",default=False)
     product_list_ids = fields.Many2many('product.product')
+
+    @api.constrains('mobile')
+    def _check_mobile_number(self):
+        for record in self:
+            if record.mobile:
+                cleaned_mobile = record.mobile.replace(" ","").replace("-","").replace("+","")
+                if not cleaned_mobile.isdigit() or len(cleaned_mobile) != 10:
+                    raise ValidationError("Mobile number should be 10 digits long and contain only numbers.")
+
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'mobile' in vals and vals_list['mobile']:
+                vals_list['mobile'] = self._format_mobile_number(vals_list['mobile'])
+        return super().create(vals_list)
+
+    def write(self, vals_list):
+        for vals in vals_list:
+            if 'mobile' in vals and vals_list['mobile']:
+                vals_list['mobile'] = self._format_mobile_number(vals_list['mobile'])
+                print(f"Formatted mobile number: {vals_list['mobile']}")
+        return super().write(vals_list)
+
+
+    def _format_mobile_number(self, mobile):
+            if mobile:
+                mobile_number = re.sub(r'[^\d]','',mobile)
+                if len(mobile_number) == 10:
+                    return f"+{mobile}"
+            return mobile
